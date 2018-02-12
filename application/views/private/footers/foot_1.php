4a228d2fc15aa205f70b4bbff53e5c99e2414241
Unknown <jesus.andres1996@hotmail.com>
2018-02-12 12:02:50 -0500

    librerias footers

--- application/views/private/footers/foot_1.php
+++ application/views/private/footers/foot_1.php
@@ -48,25 +48,22 @@
 <script src="<?= base_url('assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') ?>" type="text/javascript"></script>
 <script src="<?= base_url('assets/global/plugins/jquery.blockui.min.js" type="text/javascript') ?>"></script>
 <script src="<?= base_url('assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') ?>" type="text/javascript"></script>
-<!-- END CORE PLUGINS -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
-<script src="<?= base_url('assets/global/plugins/moment.min.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/global/plugins/fullcalendar/fullcalendar.min.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/global/plugins/jquery-ui/jquery-ui.min.js') ?>" type="text/javascript"></script>
-<!-- END PAGE LEVEL PLUGINS -->
-<!-- BEGIN THEME GLOBAL SCRIPTS -->
-
-<!-- END THEME GLOBAL SCRIPTS -->
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
 
 
-<!-- END PAGE LEVEL SCRIPTS -->
-<!-- BEGIN THEME LAYOUT SCRIPTS -->
-<script src="<?= base_url('assets/layouts/layout2/scripts/layout.min.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/layouts/layout2/scripts/demo.min.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/layouts/global/scripts/quick-sidebar.min.js') ?>" type="text/javascript"></script>
 <script src="<?= base_url('assets/layouts/global/scripts/quick-nav.min.js') ?>" type="text/javascript"></script>
-<!-- END THEME LAYOUT SCRIPTS -->
+
 <script src="<?= base_url('assets/global/scripts/datatable.js')?>" type="text/javascript"></script>
 <script src="<?= base_url('assets/global/plugins/datatables/datatables.min.js')?>" type="text/javascript"></script>
 <script src="<?= base_url('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js')?>" type="text/javascript"></script>
+<script src="<?= base_url('assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') ?>" type="text/javascript"></script>
+
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
--- application/views/private/listar_usuarios.php
+++ application/views/private/listar_usuarios.php
@@ -4,10 +4,6 @@
 <!--titulo de la pagina fin-->
 <!--librerias extras-->
 <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.16/datatables.min.css" />
-<link href="<?= base_url('assets/global/plugins/datatables/datatables.min.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') ?>" rel="stylesheet" type="text/css" />
-<link href="<?= base_url('assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') ?>" rel="stylesheet" type="text/css" />
 
 <!--librerias extras fin-->
 <!--cabeza de pagina-->
@@ -83,31 +79,81 @@
 <!-- fin contenido pies de pagina-->
 
 <!--aqui se pueden agregar ls scrips necesarios  que nesesite la pagina-->
-
-
-
-<script src="<?= base_url('assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/global/scripts/datatable.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/global/plugins/datatables/datatables.min.js') ?>" type="text/javascript"></script>
-<script src="<?= base_url('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') ?>" type="text/javascript"></script>
-<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.flash.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.html5.min.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.5.1/js/buttons.print.min.js"></script>
-
 <script>
-$(document).ready(function() {
-    $('#sample_1').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
-    } );
+jQuery(document).ready(function (){
+    var table, url;
+    table = $('#sample_1');
+    url = "{{ route('Listar_Convenios.Listar_Convenios') }}";
+    table.DataTable({
+       lengthMenu: [
+           [5, 10, 25, 50, -1],
+           [5, 10, 25, 50, "Todo"]
+       ],
+       responsive: true,
+       colReorder: true,
+       processing: true,
+       serverSide: true,
+       ajax: url,
+       searching: true,
+       language: {
+           "sProcessing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i> <span class="sr-only">Procesando...</span>',
+           "sLengthMenu": "Mostrar _MENU_ registros",
+           "sZeroRecords": "No se encontraron resultados",
+           "sEmptyTable": "Ningún dato disponible en esta tabla",
+           "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+           "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+           "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+           "sInfoPostFix": "",
+           "sSearch": "Buscar:",
+           "sUrl": "",
+           "sInfoThousands": ",",
+           "sLoadingRecords": "Cargando...",
+           "oPaginate": {
+               "sFirst": "Primero",
+               "sLast": "Último",
+               "sNext": "Siguiente",
+               "sPrevious": "Anterior"
+           },
+           "oAria": {
+               "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+               "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+           }
+       },
+       columns:[
+           {data: 'DT_Row_Index'},
+           {data: 'PK_Convenios', "visible": true, name:"documento" },
+           {data: 'Nombre', searchable: true},
+           {data: 'Fecha_Inicio', searchable: true},
+           {data: 'Fecha_Fin',searchable: true},
+           {data: 'convenios__estados.Estado', searchable: true},
+           {data: 'convenios__sedes.Sede',searchable: true},
+           {data:'action',className:'',searchable: false,
+            name:'action',
+            title:'Acciones',
+            orderable: false,
+            exportable: false,
+            printable: false,
+            defaultContent: '<a href="#" id="editar" title="Editar Convenio" class="btn btn-simple btn-warning btn-icon edit"><i class="icon-pencil"></i></a><a href="#" id="ver" title="Documentos e informacion del Convenio" class="btn btn-simple btn-success btn-icon editar2"><i class="icon-notebook"></i></a>'
+
+            
+        }
+           
+       ],
+       buttons: [
+           { extend: 'print', className: 'btn btn-circle btn-icon-only btn-default tooltips t-print', text: '<i class="fa fa-print"></i>' },
+           { extend: 'copy', className: 'btn btn-circle btn-icon-only btn-default tooltips t-copy', text: '<i class="fa fa-files-o"></i>' },
+           { extend: 'pdf', className: 'btn btn-circle btn-icon-only btn-default tooltips t-pdf', text: '<i class="fa fa-file-pdf-o"></i>',},
+           { extend: 'excel', className: 'btn btn-circle btn-icon-only btn-default tooltips t-excel', text: '<i class="fa fa-file-excel-o"></i>',},
+           { extend: 'csv', className: 'btn btn-circle btn-icon-only btn-default tooltips t-csv',  text: '<i class="fa fa-file-text-o"></i>', },
+           {text: '<i class="fa fa-refresh"></i>', className: 'btn btn-circle btn-icon-only btn-default tooltips t-refresh',
+               action: function ( e, dt, node, config ) {
+                   dt.ajax.reload();
+               }
+           }
+       ],
+       pageLength: 10,
+       dom: "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+    });
 } );</script>
 <!--fin scrips-->
 
