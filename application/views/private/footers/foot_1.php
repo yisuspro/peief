8ef1be0d20b7f34c1a26c5b7e913c645e1378327
Unknown <jesus.andres1996@hotmail.com>
2018-02-12 09:37:37 -0500

    correcion rapida

--- application/views/private/footers/foot_1.php
+++ application/views/private/footers/foot_1.php
@@ -1,21 +1,46 @@
-</a>
-</li>
-<li>
-    <a href="http://keenthemes.com/showcast/" target="_blank">
+<div class="page-footer">
+    <div class="page-footer-inner"> 2016 &copy; Metronic Theme By
+        <a target="_blank" href="http://keenthemes.com">Keenthemes</a> &nbsp;|&nbsp;
+        <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
+        <div class="scroll-to-top">
+            <i class="icon-arrow-up"></i>
+        </div>
+    </div>
+    <!-- END FOOTER -->
+    <!-- BEGIN QUICK NAV -->
+    <nav class="quick-nav">
+        <a class="quick-nav-trigger" href="#0">
+                    <span aria-hidden="true"></span>
+                </a>
+        <ul>
+            <li>
+                <a href="https://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" target="_blank" class="active">
+                            <span>Purchase Metronic</span>
+                            <i class="icon-basket"></i>
+                        </a>
+            </li>
+            <li>
+                <a href="https://themeforest.net/item/metronic-responsive-admin-dashboard-template/reviews/4021469?ref=keenthemes" target="_blank">
+                            <span>Customer Reviews</span>
+                            <i class="icon-users"></i>
+                        </a>
+            </li>
+            <li>
+                <a href="http://keenthemes.com/showcast/" target="_blank">
                             <span>Showcase</span>
                             <i class="icon-user"></i>
                         </a>
-</li>
-<li>
-    <a href="http://keenthemes.com/metronic-theme/changelog/" target="_blank">
+            </li>
+            <li>
+                <a href="http://keenthemes.com/metronic-theme/changelog/" target="_blank">
                             <span>Changelog</span>
                             <i class="icon-graph"></i>
                         </a>
-</li>
-</ul>
-<span aria-hidden="true" class="quick-nav-bg"></span>
-</nav>
-<div class="quick-nav-overlay"></div>
+            </li>
+        </ul>
+        <span aria-hidden="true" class="quick-nav-bg"></span>
+    </nav>
+    <div class="quick-nav-overlay"></div>
 </div>
 <script src="<?= base_url('assets/global/plugins/jquery.min.js') ?>" type="text/javascript"></script>
 <script src="<?= base_url('assets/global/plugins/bootstrap/js/bootstrap.min.js') ?>" type="text/javascript"></script>
