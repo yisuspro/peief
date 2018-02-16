2c3de36c139a6bb38a7c47be878ba231a3aa45f9
Unknown <jesus.andres1996@hotmail.com>
2018-02-16 11:07:01 -0500

    documentacion y comentariado de modelos

--- application/models/Logueo.php
+++ application/models/Logueo.php
@@ -1,18 +1,37 @@
 <?php
-
+/**
+*
+*@autor jesus andres castellanos aguilar
+*
+* modelor encargado de todos los procesos referente a el inicio de sesion de usuarios
+* 
+* contiene todas las consultas sql a la base de datos
+* 
+*/
 class Logueo extends CI_Model {
     
+    /**
+    * metodo constructor donde se cargan todos los helpers, librerias necesarios en el modelo
+    *@library 
+    *
+    *@helper 
+    * 
+    */
     public function __construct(){
         
     }
     
+    /**
+    * funcion para la verificacion y envio de los datos dell usuario logueado.
+    *
+    * @return row() | false
+    */
     public function login($email,$password){
-         $rep=$this->db->select('*')->join('states','users.USER_FK_state = states.STTS_PK')->join('ganders','users.USER_FK_gander = ganders.GNDR_PK')->join('types_identifications','users.USER_FK_type_identification = types_identifications.TPDI_PK')->get_where('users',array('USER_email' => $email,'USER_password'=> $password), 1);
+        $rep=$this->db->select('*')->join('states','users.USER_FK_state = states.STTS_PK')->join('ganders','users.USER_FK_gander = ganders.GNDR_PK')->join('types_identifications','users.USER_FK_type_identification = types_identifications.TPDI_PK')->get_where('users',array('USER_email' => $email,'USER_password'=> $password), 1);
         if (!$rep->result()){
             return FALSE;
         }
         $consulta =$rep;
-       
         return $consulta->row();
     }
 }
--- application/models/Users.php
+++ application/models/Users.php
@@ -1,8 +1,31 @@
 <?php
+
+/**
+*
+*@autor jesus andres castellanos aguilar
+*
+* modelor encargado de todos los procesos referente a los usuarios
+* 
+* contiene todas las consultas sql a la base de datos
+* 
+*/
 class Users extends CI_Model {
+    /**
+    * metodo constructor donde se cargan todos los helpers, librerias necesarios en el modelo
+    *@library 
+    *
+    *@helper 
+    * 
+    */
     public function __construct(){
         
     }
+    
+    /**
+    * funcion para la verificacion y envio de los datos del usuario solicitado.
+    * @param int $doc
+    * @return row() | false
+    */
     public function verificarUsuario($doc){
         $rep=$this->db->select('USER_PK')->get_where('users',array('USER_PK' => $doc), 1);
         if (!$rep->result()){
@@ -11,23 +34,46 @@ class Users extends CI_Model {
         $consulta =$rep;
         return $consulta->row();
     }
+    
+    /**
+    * funcion para el registro de usuario 
+    * @param String $datos
+    * @return true | false
+    */
     public function registrar($datos){
         if(!$this->db->insert('users',$datos)){
             return false;
         }
         return true;
     }
+    
+    /**
+    * funcion para traer todos los usuarios registrados 
+    * 
+    * @return get()
+    */
     public function listar(){
         $usuarios = $this->db->select('*')->from('users')->join('states','users.USER_FK_state = states.STTS_PK')->join('ganders','users.USER_FK_gander = ganders.GNDR_PK')->join('types_identifications','users.USER_FK_type_identification = types_identifications.TPDI_PK');
         return $usuarios->get();
     }
     
+    /**
+    * funcion para la eliminacion de un usuario 
+    * @param int $datos
+    * @return true | false
+    */
     public function eliminar($datos){
         if(!$this->db->delete('users', array('USER_PK' => $datos))){
             return FALSE;
         }
         return true;
     }
+    
+    /**
+    * funcion para traer datos especificos de un usuario 
+    * @param int $datos
+    * @return get() | false
+    */
     public function datosUsuario($datos){
         if(!$usuario= $this->db->select('*')->from('users')->where('USER_PK',$datos)->join('states','users.USER_FK_state = states.STTS_PK')->join('ganders','users.USER_FK_gander = ganders.GNDR_PK')->join('types_identifications','users.USER_FK_type_identification = types_identifications.TPDI_PK')){
             return false;
@@ -35,6 +81,12 @@ class Users extends CI_Model {
             return $usuario->get();
         }
     }
+    
+    /**
+    * funcion para la modificacion de datis de un usuario 
+    * @param int $datos | String $datos2
+    * @return true | false
+    */
     public function modificarUsuario($datos,$datos2){
         $this->db->where('USER_PK', $datos);
         if($this->db->update('users', $datos2)){
